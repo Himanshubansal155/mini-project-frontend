4172f97205af6561229903b995ec933418faa0d2
Himanshu Bansal <himanshubansal155@gmail.com>
2021-10-17 11:32:46 +0530

    installed antd icons craco and code formatted

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,44 +1,51 @@
-import React, {useState} from "react";
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined, UserOutined, UserAddOutlines } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
 
-const Header = () =>{
-    const[current, setcurrent] = useState('home')
-    const handleClick = (e) =>{
-        //console.log(e.key);
-        setcurrent(e.key);
-    }
-    return(
-        <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-        <Item key="home" icon={<AppstoreOutlined />}>
-          <Link to="/">Home</Link>
-        </Item>
+const Header = () => {
+  const [current, setcurrent] = useState("home");
+  const handleClick = (e) => {
+    setcurrent(e.key);
+  };
+  return (
+    <Menu
+      onClick={handleClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      icon={<MailOutlined />}
+    >
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
+      </Item>
 
-        <Item key="register" icon={<UserAddOutlined />} classname="float-right">
+      <Item key="register" icon={<UserAddOutlined />} classname="float-right">
         <Link to="/register">Register</Link>
-        </Item>
+      </Item>
 
-        <Item key="login" icon={<UserOutlined />} classname="float-right">
+      <Item key="login" icon={<UserOutlined />} classname="float-right">
         <Link to="/login">Login</Link>
-        </Item>
+      </Item>
 
-        
-
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
-            <Item key="setting:1">Option 1</Item>
-            <Item key="setting:2">Option 2</Item>
-          
-        </SubMenu>
-        <Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Item>
-      </Menu>
-    );
-}
+      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+      </SubMenu>
+      <Item key="alipay">
+        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+          Navigation Four - Link
+        </a>
+      </Item>
+    </Menu>
+  );
+};
 
 export default Header;
